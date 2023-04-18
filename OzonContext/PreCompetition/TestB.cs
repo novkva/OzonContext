861b3e937d026809285fd4f5b79172ee61ddd0f5
Irina Novikova <brainforfreedom@gmail.com>
2023-04-18 11:34:14 +0300

    Add project files.

---
+++ OzonContext/PreCompetition/TestB.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.WebSockets;
+
+public class TestB
+{
+    public static void TestBMain(string[] args)
+    {
+        
+        var testCaseCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i< testCaseCount; i++)
+        {
+            var count = int.Parse(Console.ReadLine());
+            var collection = Console.ReadLine().Split(' ').Select(it => int.Parse(it)).ToArray();
+            Find(collection);
+        }
+    }
+
+    public static void Find(int[] mas)
+    {
+        Dictionary<int, int> skus = new Dictionary<int, int>();
+        for (int i = 0; i < mas.Length; i++)
+        {
+            if (!skus.ContainsKey(mas[i]))
+            {
+                skus.Add(mas[i], 1);
+            }
+            else
+            {
+                skus[mas[i]] = skus[mas[i]] + 1;
+            }
+        }
+        int res = 0;
+        foreach(var d in skus)
+        {
+            res += d.Key * (d.Value - d.Value / 3);
+        }
+        Console.WriteLine(res);
+    }
+}
---
+++ OzonContext/PreCompetition/TestC.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OzonContext.PreCompetition
+{
+    internal class TestC
+    {
+        public static void Main(string[] args)
+        {
+
+            var testCaseCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testCaseCount; i++)
+            {
+                var count = int.Parse(Console.ReadLine());
+                int k = 1;
+                var collection = Console.ReadLine().Split(' ').Select(it => int.Parse(it)).ToDictionary(c => k++);
+
+                Find(collection);
+                Console.WriteLine();
+            }
+        }
+
+        public static void Find(Dictionary<int, int> mas)
+        {
+            int numb = mas.Count / 2;
+            for (int i = 0; i < numb; i++)
+            {
+                var one = mas.First();
+                mas.Remove(one.Key);
+                var two = mas.First();
+                int minraz = 100;
+                foreach (var m in mas)
+                {
+                    int now = Math.Abs(one.Value - m.Value);
+                    if (now == 0)
+                    {
+                        two = m;
+                        break;
+                    }
+                    if (now < minraz)
+                    {
+                        minraz = now;
+                        two = m;
+                    }
+                }
+                Console.WriteLine($"{one.Key} {two.Key}");
+                mas.Remove(two.Key);
+            }
+        }
+    }
+}
